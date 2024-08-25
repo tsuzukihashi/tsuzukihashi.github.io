eec27aeac4df175892e0904a0540764d5bf7244e
Ryo Tsuzukihashi <tsudukihashi0817@gmail.com>
2024-08-26 00:39:46 +0900

    Update style.js

--- js/style.js
+++ js/style.js
@@ -1,8 +1,8 @@
-fetch("header.html")
+fetch("/header.html")
   .then((response) => response.text())
   .then((data) => (document.querySelector("#header").innerHTML = data));
 
-fetch("footer.html")
+fetch("/footer.html")
   .then((response) => response.text())
   .then((data) => (document.querySelector("#footer").innerHTML = data));
 
